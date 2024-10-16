ba79f02bf56f9ec385020f8023335dbea03acaf2
Gabriel Valentoni Guelfi <gabriel.valguelfi@gmail.com>
2024-10-15 22:33:01 -0300

    add Modal component

---
+++ Modal.vue
@@ -0,0 +1,62 @@
+<template>
+  <q-dialog :persistent="persistent" v-model="show" @update:model-value="(v) => $emit('update:model-value', v)"
+    @hide="HideFn">
+    <q-card>
+      <q-toolbar :class="`bg-${themeColor} text-white`">
+        <q-avatar v-if="Icon">
+          <q-icon :name="Icon"></q-icon>
+        </q-avatar>
+
+        <q-toolbar-title v-if="Title">{{ Title }}</q-toolbar-title>
+
+        <q-btn flat round dense icon="close" v-close-popup />
+      </q-toolbar>
+
+      <!-- Sections -->
+      <div v-for="(section, name) in $slots" :key="name">
+        <q-card-section v-if="name != 'default' && name.includes('section')">
+          <slot :name="name"></slot>
+        </q-card-section>
+      </div>
+
+      <q-card-actions class="justify-center" v-if="!!Actions && Actions.length > 0">
+        <q-btn label="Cancelar" color="grey-8" icon="close" v-close-popup></q-btn>
+        <q-btn v-for="(a, i) in Actions" :key="i" :label="a.label" :color="a.color" :icon="a.icon"
+          @click="a.fn()"></q-btn>
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script>
+export default {
+  name: 'ui-layoutapp1-modal',
+
+  props: {
+    HideFn: Function,
+    Icon: String,
+    Title: String,
+    Actions: Array,
+    modelValue: Boolean,
+    persistent: Boolean
+  },
+
+  data() {
+    return {
+      show: false
+    }
+  },
+
+  watch: {
+    modelValue(v) {
+      this.show = v;
+    }
+  },
+
+  computed: {
+    themeColor() {
+      return process.env.THEME_COLOR;
+    }
+  },
+}
+</script>
